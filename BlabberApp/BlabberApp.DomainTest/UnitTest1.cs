7fec97bad22115c9ee9419c576a929275ec88878
Ryan Barlow <ryanbarlow2@mail.weber.edu>
2020-02-24 19:42:15 +0000

    Assignment 1 stuff

---
+++ BlabberApp/BlabberApp.DomainTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlabberApp.DomainTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
